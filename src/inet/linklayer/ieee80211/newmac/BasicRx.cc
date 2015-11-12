fd19984830f358052b189b393df8e45cef013db0
Levente Mészáros <levy@omnetpp.org>
2015-11-12 16:15:01 +0100

    802.11: Added received signal part change handling.

--- src/inet/linklayer/ieee80211/newmac/BasicRx.cc
+++ src/inet/linklayer/ieee80211/newmac/BasicRx.cc
@@ -82,7 +82,8 @@ void BasicRx::lowerFrameReceived(Ieee80211Frame *frame)
 
 bool BasicRx::isReceptionInProgress() const
 {
-    return receptionState == IRadio::RECEPTION_STATE_RECEIVING;
+    return receptionState == IRadio::RECEPTION_STATE_RECEIVING &&
+           (receivedPart == IRadioSignal::SIGNAL_PART_WHOLE || receivedPart == IRadioSignal::SIGNAL_PART_DATA);
 }
 
 bool BasicRx::isFcsOk(Ieee80211Frame *frame) const
@@ -106,6 +107,13 @@ void BasicRx::receptionStateChanged(IRadio::ReceptionState state)
 {
     Enter_Method_Silent();
     receptionState = state;
+    recomputeMediumFree();
+}
+
+void BasicRx::receivedSignalPartChanged(IRadioSignal::SignalPart part)
+{
+    Enter_Method_Silent();
+    receivedPart = part;
     recomputeMediumFree();
 }
 
@@ -155,7 +163,6 @@ void BasicRx::updateDisplayString()
                 case IRadio::RECEPTION_STATE_IDLE: os << "Rx-Idle"; break; // cannot happen
                 case IRadio::RECEPTION_STATE_BUSY: os << "Noise"; break;
                 case IRadio::RECEPTION_STATE_RECEIVING: os << "Recv"; break;
-                case IRadio::RECEPTION_STATE_SYNCHRONIZING: os << "Syncing"; break;
             }
             addSpace = true;
         }
